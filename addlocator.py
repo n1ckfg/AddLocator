165d7d81cd54b02f69e69190f0156d0ef1d0a3a8
Nick Fox-Gieg <nick@fox-gieg.com>
2013-08-05 16:26:37 -0600

    Add locators for child joints

--- addlocator.py
+++ addlocator.py
@@ -6,12 +6,23 @@ target = ls(sl=True)
 #2. for each selected object...
 for i in range(0,len(target)):
 	
-	#3. ...get its name
+	#3. ...if there are child joints, give them locators too
+	try:
+		kids = listRelatives(ls(selection=True), children=True, type="joint", allDescendents=True)
+		for k in kids:
+			locName = k + "_loc"
+			locPos = xform(k, q=True, t=True, ws=True)
+	   		loc = spaceLocator(n=locName)
+	   		move(locPos[0],locPos[1],locPos[2])
+	except:
+		print "No child joints."
+
+	#4. get the original selection's name
 	locName = target[i] + "_loc"
 	
-	#4. get its position
+	#5. get its position
 	locPos = xform(target[i], q=True, t=True, ws=True)
 	
-	#5. create a new locator with that name at that position
+	#6. create a new locator with that name at that position
 	loc = spaceLocator(n=locName)
 	move(locPos[0],locPos[1],locPos[2])
