fe82a4b55245d3e8b82a15c7a209428c6ee2b73d
Nick Fox-Gieg <nick@fox-gieg.com>
2013-08-10 13:45:04 -0400

    replace now works with keyframes

--- menu-replacefirst.py
+++ menu-replacefirst.py
@@ -6,16 +6,24 @@ target = ls(sl=True)
 #2. for each selected object after the first...
 for i in range(1,len(target)):
 
-	#3. duplicate the first selection
-	select(target[0])
-	duplicate()
+    #3. ...get current selection's position and copy keyframes
+    select(target[i])
+    pos = xform(target[i], q=True, t=True, ws=True)
+    try:
+        copyKey()
+    except:
+        print "Couldn't copy keys."
 
-	#4. ...get the position
-	pos = xform(target[i], q=True, t=True, ws=True)
+    #4. duplicate the first selection
+    select(target[0])
+    duplicate()
     
-	
-	#5. move to position
-	move(pos[0],pos[1],pos[2])
-
-	#6. delete selection
-	delete(target[i])
+    #5. move first selection to position and paste keyframes
+    move(pos[0],pos[1],pos[2])
+    try:
+        pasteKey()
+    except:
+        print "Couldn't paste keys."
+       
+    #6. delete selection
+    delete(target[i])
