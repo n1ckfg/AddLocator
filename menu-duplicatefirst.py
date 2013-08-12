eb69159ddf499c1ba6770328a5aeacc0e376df85
Nick Fox-Gieg <nick@fox-gieg.com>
2013-08-11 23:08:03 -0400

    bug fix

--- menu-duplicatefirst.py
+++ menu-duplicatefirst.py
@@ -10,28 +10,36 @@ if(len(target)==1):
     mel.eval("duplicate -un -ic -rc")
 
 else:
-    #3. ...otherwise, for each selected object...
-    for i in range(1,len(target)):
-
-        #4. ...get current selection's position and copy keyframes
-        select(target[i])
-        pos = xform(target[i], q=True, t=True, ws=True)
-        try:
-            copyKey()
-        except:
-            print "Couldn't copy keys."
-
-        #5. duplicate the first selection
+    try:
+        #3. check if the first selection is skinned.
         select(target[0])
-        #call through mel because python has no rc option!
-        mel.eval("duplicate -un -ic -rc")
-        
-        #6. move first selection to position and paste keyframes
-        move(pos[0],pos[1],pos[2])
-        try:
-            pasteKey()
-        except:
-            print "Couldn't paste keys."
-           
-        #7. delete selection
-        delete(target[i])
+        skinCluster(q=True)
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        print "Select the root joint for this to work properly."
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    except:
+        #4. ...otherwise, for each selected object...
+        for i in range(1,len(target)):
+
+            #5. ...get current selection's position and copy keyframes
+            select(target[i])
+            pos = xform(target[i], q=True, t=True, ws=True)
+            try:
+                copyKey()
+            except:
+                print "Couldn't copy keys."
+
+            #6. duplicate the first selection
+            select(target[0])
+            #call through mel because python has no rc option!
+            mel.eval("duplicate -un -ic -rc")
+            
+            #7. move first selection to position and paste keyframes
+            move(pos[0],pos[1],pos[2])
+            try:
+                pasteKey()
+            except:
+                print "Couldn't paste keys."
+               
+            #8. delete selection
+            delete(target[i])
